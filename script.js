102b9d309d2293d608afc2cf6d91937251cd3a8e
deborapeixotoda <134522826+deborapeixotoda@users.noreply.github.com>
2023-11-10 12:13:46 -0300

    prova pt 3

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ function mudarTexto() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
 
-function tocaSomTheweeknd()
+function myalert(){
+    alert("voce clicou nesse botão")
+}
+function tocaSomTheweeknd(){
+    document.querySelector('#som_tecla_theweeknd').play();
+}
+document.querySelector('.tecla_theweeknd').onclick = tocaSomTheweeknd;
